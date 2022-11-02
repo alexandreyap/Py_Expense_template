3cb1a6b2e39a407b223b1463c580a1ef572bfe0e
alexandreyap <alexandre.yap@epita.fr>
2022-11-02 19:39:51 +0100

    Create user feature

--- user.py
+++ user.py
@@ -1,8 +1,31 @@
 from PyInquirer import prompt
-user_questions = [
+import csv
 
+user_questions = [
+{
+        "type":"input",
+        "name":"spender",
+        "message":"New User - Name: ",
+    },
 ]
 
-def add_user():
+def add_user(*args):
     # This function should create a new user, asking for its name
+    user = prompt(user_questions)
+    userName = user['spender']
+
+    # Check if the user already exists
+    with open('users.csv', newline='') as csvfile:
+        spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+        for row in spamreader:
+            if userName in row:
+                print('User already exists')
+                return
+
+    # Add the user to the users.csv file
+    with open('users.csv', 'a', newline='') as csvfile:
+        spamwriter = csv.writer(csvfile, delimiter=' ', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        spamwriter.writerow([userName])
+
+    print(f"User {userName} Added !")
     return
