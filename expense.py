2d8205085b2b29608f17d7b7428cafd4e4bf8906
alexandreyap <alexandre.yap@epita.fr>
2022-11-02 19:46:56 +0100

    Add expense in expenses.csv file

--- expense.py
+++ expense.py
@@ -1,4 +1,5 @@
 from PyInquirer import prompt
+import csv
 
 expense_questions = [
     {
@@ -23,7 +24,30 @@ expense_questions = [
 
 def new_expense(*args):
     infos = prompt(expense_questions)
-    # Writing the informations on external file might be a good idea ¯\_(ツ)_/¯
+
+    amount = infos['amount']
+    label = infos['label']
+    spender = infos['spender']
+
+    # Check if spender exists
+    exists = False
+    with open('users.csv', newline='') as csvfile:
+        spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+        for row in spamreader:
+            if spender in row:
+                exists = True
+                break
+    
+    if not exists:
+        print('Spender does not exist, please add it first')
+        return
+
+    # Add the expense to the expenses.csv file
+    with open('expenses.csv', 'a', newline='') as csvfile:
+        spamwriter = csv.writer(csvfile, delimiter=' ', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        spamwriter.writerow([amount, label, spender])
+
+
     print("Expense Added !")
     return True
 
